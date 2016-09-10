3efb86af33ac38f7cb95f2253c4ef1d84b8443d8
Akifumi S. Tanabe <akifumi.tanabe+github@gmail.com>
2016-09-10 09:46:22 +0900

    Update English version

--- converthtml2html.ja.pl
+++ converthtml2html.ja.pl
@@ -23,6 +23,20 @@ while (<>) {
 		$switch = 0;
 		s/^<\/span>/<\/p>/;
 	}
+	if (/<div class="ltx_date ltx_role_creation">/) {
+		s/January (\d+), (\d+)/$2年1月$1日/;
+		s/February (\d+), (\d+)/$2年2月$1日/;
+		s/March (\d+), (\d+)/$2年3月$1日/;
+		s/April (\d+), (\d+)/$2年4月$1日/;
+		s/May (\d+), (\d+)/$2年5月$1日/;
+		s/June (\d+), (\d+)/$2年6月$1日/;
+		s/July (\d+), (\d+)/$2年7月$1日/;
+		s/August (\d+), (\d+)/$2年8月$1日/;
+		s/September (\d+), (\d+)/$2年9月$1日/;
+		s/October (\d+), (\d+)/$2年10月$1日/;
+		s/November (\d+), (\d+)/$2年11月$1日/;
+		s/December (\d+), (\d+)/$2年12月$1日/;
+	}
 	s/(<cite class="ltx_cite">)(.+?)(<a href="\#bib\.bib\d+" title="" class="ltx_ref">)(.+?)(<\/a>)(.+?)(<\/cite>)/$1$3$2$4$6$5$7/g;
 	s/<li id="bib.bib\d+" class="ltx_bibitem"/$& style="padding:0em 0em 0.2em 0em;"/;
 	s/<ul class="ltx_biblist"/$& style="padding:0em 0em 0em 1em;text-indent:-1em;"/;
